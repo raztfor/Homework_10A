4b551db15bcf618461cbdd02ce87b21d2753190f
raztfor <59148328+raztfor@users.noreply.github.com>
2019-12-22 22:12:56 +0200

    Add files via upload

---
+++ Brad_Camincean_Razvan (1).java
@@ -0,0 +1,18 @@
+package Bradut;
+
+public class Bradut {
+
+  public static void main(String[] args) {
+
+char a = '#';
+char b = '*';
+char d = 'I';
+System.out. format ("%8c%n", a);
+System.out. format("%6c %c %c %n", a, a, a);
+System.out. format ("%4c %c %c %c %c %n", a, a, b, a, a);
+System.out. format ("%3c %c %c %c %c %c %n", a, b, a, a, b, a);
+System.out.format ("%4c %c %c %c %c %n", a, a, b, a, a);
+System.out. format ("%6c %c %c %n", a, a, a);
+System.out. format ("%7c %c", d, d);
+  }
+}
